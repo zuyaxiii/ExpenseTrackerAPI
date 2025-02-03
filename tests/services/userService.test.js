9fdfddb2ab9a6791da6d55b0ac2175e6a8f11aa3
zuyaxiii <akiratta1810@gmail.com>
2025-02-03 12:16:32 +0700

    fix bug

--- tests/services/userService.test.js
+++ tests/services/userService.test.js
@@ -9,7 +9,7 @@ describe('User Service', () => {
     let userData;
 
     beforeAll(async () => {
-        await mongoose.connect(process.env.MONGO_URI_TEST, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
